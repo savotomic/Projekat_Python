7a7d563599d2c78ffe7cdbb0a358f399c1d7df27
Aleksandar Mijatovic <acamijatovic.98@gmail.com>
2020-02-24 21:03:30 +0200

    #set

---
+++ Set.py
@@ -0,0 +1,39 @@
+class Set:
+
+    def __init__(self):
+        self.rezultat = {}
+
+    def presek(self,prvaRec,drugaRec):
+        rezultat = Set()
+        for stranica1, rang1 in prvaRec.items():
+            for stranica2, rang2 in drugaRec.items():
+                if stranica1 == stranica2:
+                    rezultat[stranica1] = rang1 + rang2
+
+        return recnikNovi
+
+
+    def unija(self,prvaRec,drugaRec):
+        rezultat = Set()
+        for stranica1, rang in prvaRec.items():
+            if stranica1 not in drugaRec:
+                rezultat[stranica1] = rang
+
+        for stranica1, rang1 in prvaRec.items():
+            for stranica2, rang2 in drugaRec.items():
+                if stranica1 == stranica2:
+                    rezultat[stranica1] = rang1 + rang2
+
+        for stranica2, rang in drugaRec.items():
+            if stranica2 not in drugaRec:
+                rezultat[stranica2] = rang
+
+        return rezultat
+
+    def komplement(self,prvaRec,drugaRec):
+        rezultat = Set()
+        for stranica1, rang in prvaRec.items():
+            if stranica1 not in drugaRec:
+                rezultat[stranica1] = rang
+
+        return rezultat
