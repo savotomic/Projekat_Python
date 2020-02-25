add8b48b5b77fc85ac37ff31e8d707aa16d9cb5f
Aleksandar Mijatovic <acamijatovic.98@gmail.com>
2020-02-25 21:16:54 +0100

    #malo_izmenjena_klasa_set

--- Set.py
+++ Set.py
@@ -3,38 +3,31 @@ class Set:
     def __init__(self):
         self.rezultat = {}
 
-    def presek(self,prvaRec,drugaRec):
-        rezultat = {}
-        for stranica1, rang1 in prvaRec.items():
-            for stranica2, rang2 in drugaRec.items():
-                if stranica1 == stranica2:
-                    rezultat[stranica1] = rang1 + rang2
-
-        return rezultat
-
-
-    def unija(self,prvaRec,drugaRec):
-        rezultat = {}
-
-        for stranica1, rang in prvaRec.items():
-            if stranica1 not in drugaRec:
-                rezultat[stranica1] = rang
+    def presek(self, prvaRec, drugaRec):
 
         for stranica1, rang1 in prvaRec.items():
             for stranica2, rang2 in drugaRec.items():
                 if stranica1 == stranica2:
-                    rezultat[stranica1] = rang1 + rang2
+                    self.rezultat[stranica1] = rang1 + rang2
 
-        for stranica2, rang in drugaRec.items():
-            if stranica2 not in drugaRec:
-                rezultat[stranica2] = rang
+    def unija(self, prvaRec, drugaRec):
 
-        return rezultat
+        for stranica1, value in prvaRec.items():
+            for stranica2, value2 in drugaRec.items():
+                if stranica1 == stranica2:
+                    self.rezultat[stranica1] = value + value2
 
-    def komplement(self,prvaRec,drugaRec):
-        rezultat = {}
+        for stranica1, value in prvaRec.items():
+            if stranica1 not in drugaRec:
+                self.rezultat[stranica1] = value
+
+        for stranica2, value in drugaRec.items():
+            if stranica2 not in prvaRec:
+                self.rezultat[stranica2] = value
+
+    def komplement(self, prvaRec, drugaRec):
+
         for stranica1, rang in prvaRec.items():
             if stranica1 not in drugaRec:
-                rezultat[stranica1] = rang
+                self.rezultat[stranica1] = rang
 
-        return rezultat
